49c55aed6576b667e7030f1a490060602a79d67e
vundavalliprem <80057416+vundavalliprem@users.noreply.github.com>
2025-12-07 23:02:57 +0530

    Update feature path in TestRunner class
    Update feature path in TestRunner class

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 		tags="",
-        features = "src/test/resources/features/myInfoPersonalDetails.feature",
+        features = "src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
         plugin = {"pretty", "html:target/cucumber-reports/cucumber.html", "json:target/cucumber-reports/cucumber.json"}
 )
